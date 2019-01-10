8d6a775a839478334e705e4e07ae874709e91964
zqb <zhangqianbin3@foxmail.com>
2019-01-10 18:02:23 +0800

    808优化

--- app/src/main/java/com/guoyee/haisi808/TCP808Service.kt
+++ app/src/main/java/com/guoyee/haisi808/TCP808Service.kt
@@ -14,9 +14,10 @@ import org.greenrobot.eventbus.ThreadMode
 import java.io.IOException
 import java.io.OutputStream
 import java.net.*
-import java.util.ArrayList
+import java.util.*
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
+import java.util.concurrent.LinkedBlockingQueue
 import kotlin.experimental.and
 
 /**
@@ -27,6 +28,9 @@ class TCP808Service: Service() {
     private lateinit var mSingleThreadExecutor: ExecutorService
     private val mTcpPort: Int = 8888
     private val mUdpPort: Int = 9999
+
+    private var mCmdQueue: LinkedBlockingQueue<ByteArray> = LinkedBlockingQueue()
+
 
     private var mIp = "192.168.1.99"
 
@@ -70,8 +74,6 @@ class TCP808Service: Service() {
     inner class TcpReceiveThread: Thread() {
 
         private lateinit var mTcpServerSocket: ServerSocket
-        private var mBufferList: ArrayList<Byte> = ArrayList()
-        private var mRecLen: Int = 0
 
         override fun run() {
             super.run()
@@ -83,18 +85,17 @@ class TCP808Service: Service() {
                     ToastUtils.showLong("客户端已经成功连接")
                     Thread(Runnable {
                         try {
+                            CmdParseThread(socket).start()
                             while (!socket.isClosed) {
                                 val buf = ByteArray(1024)
                                 val len = socket.getInputStream().read(buf)
 //                                LogUtils.e(ConvertUtils.bytes2HexString(buf))
-                                ToastUtils.showLong(ConvertUtils.bytes2HexString(buf))
+//                                ToastUtils.showLong(ConvertUtils.bytes2HexString(buf))
                                 if (len > 0) {
-                                    for (i in 0 until len) {
-                                        mBufferList.add(buf[i])
-                                    }
-                                    mRecLen += len
+                                    val byt = ByteArray(len)
+                                    System.arraycopy(buf, 0, byt, 0, len)
+                                    mCmdQueue.offer(byt)
                                 }
-                                parseCmd(socket)
                             }
                         } catch (e: IOException) {
                             e.printStackTrace()
@@ -105,68 +106,105 @@ class TCP808Service: Service() {
                 }
             }
         }
+    }
 
-        private fun parseCmd(socket: Socket) {
-            if (Bytes.toArray(mBufferList)[0] == 0x7e.toByte() && Bytes.toArray(mBufferList)[1] != 0x7e.toByte()) {
-                var countLen: Int
-                val msgId = byteArrayOf(Bytes.toArray(mBufferList)[1], Bytes.toArray(mBufferList)[2])
-                if (ConvertUtils.bytes2HexString(msgId) == "0100") {
-                    mSingleThreadExecutor.execute(TcpSend8100Thread(socket.getOutputStream()))
-                }
-                if (ConvertUtils.bytes2HexString(msgId) == "0102") {
-                    mSingleThreadExecutor.execute(TcpSend8001Thread(socket.getOutputStream()))
-                    mSingleThreadExecutor.execute(TcpSend9101Video1(socket.getOutputStream()))
-                    mSingleThreadExecutor.execute(TcpSend9101Video2(socket.getOutputStream()))
-                    mSingleThreadExecutor.execute(TcpSend9101Video3(socket.getOutputStream()))
-                    mSingleThreadExecutor.execute(TcpSend9101Video4(socket.getOutputStream()))
-                }
-                var msgLen = ConvertUtilsPlus.short2Unsigned(Bytes.toArray(mBufferList)[4].toShort())
-//                msgLen += Bytes.toArray(mBufferList)[3] and 0x03.toByte()
-                msgLen += ConvertUtilsPlus.short2Unsigned((Bytes.toArray(mBufferList)[3] and 0x03.toByte()).toShort()) shl 8
-//                LogUtils.e(ConvertUtils.bytes2HexString(Bytes.toArray(mBufferList)))
-//                LogUtils.e(msgLen)
-                var encryptType = ConvertUtilsPlus.short2Unsigned(Bytes.toArray(mBufferList)[3].toShort()) shr 1
-                encryptType = encryptType and 0x07
-                var pkgType = ConvertUtilsPlus.short2Unsigned(Bytes.toArray(mBufferList)[3].toShort()) shr 4
-                pkgType = pkgType and 0x01
-                val phone = byteArrayOf(
-                    Bytes.toArray(mBufferList)[5], Bytes.toArray(mBufferList)[6], Bytes.toArray(mBufferList)[7],
-                    Bytes.toArray(mBufferList)[8], Bytes.toArray(mBufferList)[9], Bytes.toArray(mBufferList)[10])
-                val msgNum = byteArrayOf(Bytes.toArray(mBufferList)[11], Bytes.toArray(mBufferList)[12])
+    inner class CmdParseThread(private var socket: Socket) : Thread() {
 
-                countLen = if (pkgType == 0) {
-                    13
-                } else {
-                    17
-                }
-                val msgBody = ByteArray(msgLen)
-                System.arraycopy(Bytes.toArray(mBufferList), countLen, msgBody, 0, msgLen)
-                val checkCode = Bytes.toArray(mBufferList)[countLen + msgLen]
+        private var mCmdBufferList: ArrayList<Byte> = ArrayList()
 
-//                LogUtils.e("" + ConvertUtils.bytes2HexString(msgId) + " " + ConvertUtils.bytes2HexString(phone) +
-//                        " " + msgLen + " " + encryptType + " " + pkgType + " " + ConvertUtils.bytes2HexString(msgNum) +
-//                        " " + ConvertUtils.bytes2HexString(msgBody) + " " + Integer.toHexString(checkCode.toInt()))
+        override fun run() {
+            super.run()
+            LogUtils.e("TCP808命令解析线程已开启")
+            while (true) {
+                if (mCmdQueue.size > 0) {
+                    val poll = mCmdQueue.poll()
+                    if (poll == null) {
+                        LogUtils.e("空")
+                        continue
+                    }
 
-                countLen += msgLen + 2
-                mRecLen -= countLen
-                for (i in 0 until countLen) {
-                    mBufferList.removeAt(0)
+                    mCmdBufferList.addAll(poll.toTypedArray())
+
+                    if (mCmdBufferList.size in 0..17) {
+                        continue
+                    }
+                    if (Bytes.toArray(mCmdBufferList)[0] == 0x7e.toByte()) {
+                        var pkgLen: Int
+                        var headLen: Int
+                        var msgLen = ConvertUtilsPlus.short2Unsigned(Bytes.toArray(mCmdBufferList)[4].toShort())
+                        msgLen += ConvertUtilsPlus.short2Unsigned((Bytes.toArray(mCmdBufferList)[3] and 0x03.toByte()).toShort()) shl 8
+                        var pkgType = ConvertUtilsPlus.short2Unsigned(Bytes.toArray(mCmdBufferList)[3].toShort()) shr 4
+                        pkgType = pkgType and 0x01
+
+                        headLen = if (pkgType == 0) {
+                            13
+                        } else {
+                            17
+                        }
+                        pkgLen = headLen + msgLen + 2
+                        if (pkgLen > mCmdBufferList.size) {
+                            continue
+                        }
+                        val pkg = ByteArray(pkgLen)
+                        System.arraycopy(Bytes.toArray(mCmdBufferList), 0, pkg, 0, pkg.size)
+                        LogUtils.e(ConvertUtils.bytes2HexString(pkg))
+                        ToastUtils.showShort(ConvertUtils.bytes2HexString(pkg))
+
+                        val msgId = byteArrayOf(pkg[1], pkg[2])
+                        if (ConvertUtils.bytes2HexString(msgId) == "0100") {
+                            mSingleThreadExecutor.execute(TcpSend8100Thread(socket.getOutputStream()))
+                        }
+                        if (ConvertUtils.bytes2HexString(msgId) == "0102") {
+                            mSingleThreadExecutor.execute(TcpSend8001Thread(socket.getOutputStream()))
+
+                            mSingleThreadExecutor.execute(TcpSend9101Video1(socket.getOutputStream()))
+                            mSingleThreadExecutor.execute(TcpSend9101Video2(socket.getOutputStream()))
+                            mSingleThreadExecutor.execute(TcpSend9101Video3(socket.getOutputStream()))
+                            mSingleThreadExecutor.execute(TcpSend9101Video4(socket.getOutputStream()))
+                        }
+
+                        var encryptType = ConvertUtilsPlus.short2Unsigned(pkg[3].toShort()) shr 1
+                        encryptType = encryptType and 0x07
+
+
+                        val phone = byteArrayOf(pkg[5], pkg[6], pkg[7], pkg[8], pkg[9], pkg[10])
+
+                        val msgNum = byteArrayOf(pkg[11], pkg[12])
+
+
+                        val msgBody = ByteArray(msgLen)
+                        System.arraycopy(pkg, headLen, msgBody, 0, msgLen)
+
+                        val checkCode = pkg[headLen + msgLen]
+
+
+
+//                        LogUtils.e("" + ConvertUtils.bytes2HexString(msgId) + " " + ConvertUtils.bytes2HexString(phone) +
+//                                " " + msgLen + " " + encryptType + " " + pkgType + " " + ConvertUtils.bytes2HexString(msgNum) +
+//                                " " + ConvertUtils.bytes2HexString(msgBody) + " " + Integer.toHexString(checkCode.toInt()))
+
+
+                        val array = ByteArray(mCmdBufferList.size - pkgLen)
+                        System.arraycopy(Bytes.toArray(mCmdBufferList), pkgLen, array, 0, array.size)
+
+                        mCmdBufferList.clear()
+                        mCmdBufferList.addAll(array.toTypedArray())
+                    } else {
+                        val bytes2HexString = ConvertUtils.bytes2HexString(Bytes.toArray(mCmdBufferList))
+                        val list = bytes2HexString.split("7e")
+//                        LogUtils.e(mVideoBufferList.size.toString() + "----" + bytes2HexString + "----" + list[0] + "----" + list[1])
+                        LogUtils.e("清空:" + bytes2HexString.length + " " + list[0].length)
+                        mCmdBufferList.clear()
+                        mCmdBufferList.addAll(ConvertUtils.hexString2Bytes("7e" + list[1]).toTypedArray())
+                    }
                 }
-//                LogUtils.e("" + mBufferList.size)
-            } else {
-                mRecLen--
-                mBufferList.removeAt(0)
             }
         }
     }
 
+
     inner class TcpSend9101Video1(var outputStream: OutputStream) : Thread() {
-
-        private val mCmd = ConvertUtils.hexString2Bytes(("7E 9101 0005 013900139001 001D" +
-                "ff 0102 00" + "207E").replace(" ", ""))
-
         private var mList: ArrayList<Byte> = ArrayList()
-
         init {
             mList.add(0x7e)
             mList.addAll(ConvertUtils.hexString2Bytes("91010005013900139001001D").toTypedArray())
@@ -184,10 +222,8 @@ class TCP808Service: Service() {
             mList[4] = (mList.size - 15).toByte()
             mList[mList.size - 2] = ConvertUtilsPlus.getXOR(mList)/**/
         }
-
         override fun run() {
             super.run()
-//            LogUtils.e("已发送数据：" + ConvertUtils.bytes2HexString(ConvertUtils.hexString2Bytes("7E0100002E7E")))
             LogUtils.e("已发送数据：" + ConvertUtils.bytes2HexString(Bytes.toArray(mList)))
             LogUtils.e("已发送数据：" + String(Bytes.toArray(mList)))
             outputStream.write(Bytes.toArray(mList))
@@ -195,12 +231,7 @@ class TCP808Service: Service() {
     }
 
     inner class TcpSend9101Video2(var outputStream: OutputStream) : Thread() {
-
-        private val mCmd = ConvertUtils.hexString2Bytes(("7E 9101 0005 013900139001 001D" +
-                "ff 0102 00" + "207E").replace(" ", ""))
-
         private var mList: ArrayList<Byte> = ArrayList()
-
         init {
             mList.add(0x7e)
             mList.addAll(ConvertUtils.hexString2Bytes("91010005013900139001001D").toTypedArray())
@@ -221,7 +252,6 @@ class TCP808Service: Service() {
 
         override fun run() {
             super.run()
-//            LogUtils.e("已发送数据：" + ConvertUtils.bytes2HexString(ConvertUtils.hexString2Bytes("7E0100002E7E")))
             LogUtils.e("已发送数据：" + ConvertUtils.bytes2HexString(Bytes.toArray(mList)))
             LogUtils.e("已发送数据：" + String(Bytes.toArray(mList)))
             outputStream.write(Bytes.toArray(mList))
@@ -229,12 +259,7 @@ class TCP808Service: Service() {
     }
 
     inner class TcpSend9101Video3(var outputStream: OutputStream) : Thread() {
-
-        private val mCmd = ConvertUtils.hexString2Bytes(("7E 9101 0005 013900139001 001D" +
-                "ff 0102 00" + "207E").replace(" ", ""))
-
         private var mList: ArrayList<Byte> = ArrayList()
-
         init {
             mList.add(0x7e)
             mList.addAll(ConvertUtils.hexString2Bytes("91010005013900139001001D").toTypedArray())
@@ -255,7 +280,6 @@ class TCP808Service: Service() {
 
         override fun run() {
             super.run()
-//            LogUtils.e("已发送数据：" + ConvertUtils.bytes2HexString(ConvertUtils.hexString2Bytes("7E0100002E7E")))
             LogUtils.e("已发送数据：" + ConvertUtils.bytes2HexString(Bytes.toArray(mList)))
             LogUtils.e("已发送数据：" + String(Bytes.toArray(mList)))
             outputStream.write(Bytes.toArray(mList))
@@ -263,12 +287,7 @@ class TCP808Service: Service() {
     }
 
     inner class TcpSend9101Video4(var outputStream: OutputStream) : Thread() {
-
-        private val mCmd = ConvertUtils.hexString2Bytes(("7E 9101 0005 013900139001 001D" +
-                "ff 0102 00" + "207E").replace(" ", ""))
-
         private var mList: ArrayList<Byte> = ArrayList()
-
         init {
             mList.add(0x7e)
             mList.addAll(ConvertUtils.hexString2Bytes("91010005013900139001001D").toTypedArray())
@@ -289,7 +308,6 @@ class TCP808Service: Service() {
 
         override fun run() {
             super.run()
-//            LogUtils.e("已发送数据：" + ConvertUtils.bytes2HexString(ConvertUtils.hexString2Bytes("7E0100002E7E")))
             LogUtils.e("已发送数据：" + ConvertUtils.bytes2HexString(Bytes.toArray(mList)))
             LogUtils.e("已发送数据：" + String(Bytes.toArray(mList)))
             outputStream.write(Bytes.toArray(mList))
